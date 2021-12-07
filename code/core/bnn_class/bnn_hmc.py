27ad8b468b0b0317c3890cf66812657be825c51c
reneaas <reneaas@uio.no>
2021-12-07 22:00:29 +0100

    made a expandable class for bnn with hmc

---
+++ code/core/bnn_class/bnn_hmc.py
@@ -0,0 +1,229 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tqdm import trange
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+np.random.seed(1)
+tf.random.set_seed(1)
+
+
+class BayesianNeuralNetworkHMC:
+    def __init__(
+        self,
+        layers=None,
+        activation=None,
+        top_layer_activation=tf.identity,
+        kernel_prior=None,
+        bias_prior=None,
+        prior_mean=0.0,
+        prior_stddev=0.01,
+        lamb=0.0,
+    ):
+        self.lamb = lamb  # Regularization parameter.
+        # Set priors of kernel
+        if kernel_prior:
+            self.kernel_prior = kernel_prior
+        else:
+            self.kernel_prior = tfp.distributions.Normal(
+                loc=prior_mean, scale=prior_stddev
+            )
+
+        # Set priors of bias
+        if bias_prior:
+            self.bias_prior = bias_prior
+        else:
+            self.bias_prior = tfp.distributions.Normal(
+                loc=prior_mean, scale=prior_stddev
+            )
+
+        # Get initial parameters, if layers are provided.
+        if layers:
+            tmp = [
+                (
+                    self.kernel_prior.sample(sample_shape=(n, m)),
+                    self.bias_prior.sample(sample_shape=(m,)),
+                )
+                for n, m in zip(layers[:-1], layers[1:])
+            ]
+
+            self.weights = []
+            for weights in tmp:
+                kernel, bias = weights
+                self.weights.append(kernel)
+                self.weights.append(bias)
+            del tmp  # Delete reference to tmp. Served its purpose.
+
+        if activation:
+            self.activation = activation
+        else:
+            self.activation = tf.nn.sigmoid
+
+        self.top_layer_activation = top_layer_activation
+
+    @tf.function
+    def prior_log_prob_fn(self, weights):
+        """Log prior probability function of the weights of the network.
+        Currently set to be exp(-lamb * w ** 2) according to Radfor Neals treatment
+        of BNNs. `lamb` is the analogue to a regularization
+        hyperparameter in L2-regularization and serves the same purpose for BNNs.
+
+        Args:
+            weights (list[tf.Tensor])   :   List containing tensors with kernels and biases.
+        """
+        log_prob = 0
+        for w in weights:
+            log_prob -= tf.reduce_sum(w ** 2)
+        return self.lamb * log_prob
+
+    def log_likelihood(self, x, y, weights):
+        """Computes log likelihood of predicted target y given features `x` and `weights`.
+
+        Args:
+            x  (tf.Tensor)              :   Input features of shape [num_points, num_features]
+            y   (tf.Tensor)             :   Predicted targets of shape [num_points, num_outputs]
+            weights (list[tf.Tensor])   :   list of weights of the network.
+
+        Returns:
+            The log likelihood of yhat as the mean value given target y.
+            Equivalent to the residual sum of squares (RSS).
+        """
+        model = self.build_model(weights)
+        yhat = model(x)
+        return -0.5 * tf.reduce_sum((y - yhat) ** 2)
+
+    def create_log_prob_fn(self, x, y):
+        """Returns the combines log probability function needed to
+        use tf.mcmc.sample_chain.
+
+        Args:
+            x (tf.Tensor)   :   Input features of shape [num_points, num_features].
+                                Usually only used with training data.
+            y (tf.Tensor)   :   Predicted targets of shape [num_points, num_outputs].
+                                Usually only used with training data.
+
+        Returns:
+            target_log_prob_fn (target log probability function used in the MCMC chain).
+        """
+
+        def target_log_prob_fn(*weights):
+            log_prob = self.prior_log_prob_fn(weights) + self.log_likelihood(
+                x, y, weights
+            )
+            return log_prob
+
+        return target_log_prob_fn
+
+    def build_model(self, weights):
+        """Creates a feed-forward densely connected neural network
+        given input weights.
+
+        Args:
+            weights (list)  :   List containing the weights of the network.
+
+        Returns:
+            model (function)    :   Python callable that computes a forward pass
+                                    of the densely connected neural network.
+        """
+
+        def model(x):
+            kernel = weights[::2]
+            bias = weights[1::2]
+            for w, b in zip(kernel[:-1], bias[:-1]):
+                x = self.activation(tf.matmul(x, w) + b)
+            x = self.top_layer_activation(tf.matmul(x, kernel[-1]) + bias[-1])
+            return x
+
+        return model
+
+    def predict_from_chain(self, x, chain=None):
+        predictions = []
+        if chain:
+            for weights in chain:
+                model = self.build_model(weights)
+                predictions.append(model(x).numpy())
+        else:
+            for weights in self.chain:
+                model = self.build_model(weights)
+                predictions.append(model(x).numpy())
+        return predictions
+
+    @tf.function
+    def sample_chain(self, *args, **kwargs):
+        return tfp.mcmc.sample_chain(*args, **kwargs)
+
+    def hmc_chain(
+        self, x, y, num_results, num_burnin_steps, num_leapfrog_steps, step_size
+    ):
+        current_state = self.weights
+        target_log_prob_fn = self.create_log_prob_fn(x, y)
+        kernel = tfp.mcmc.HamiltonianMonteCarlo(
+            target_log_prob_fn=target_log_prob_fn,
+            step_size=step_size,
+            num_leapfrog_steps=num_leapfrog_steps,
+            store_parameters_in_results=None,
+        )
+
+        self.chain = self.sample_chain(
+            kernel=kernel,
+            current_state=current_state,
+            num_results=num_results,
+            num_burnin_steps=num_burnin_steps,
+            num_steps_between_results=0,
+            trace_fn=None,
+        )
+
+        self.chain = [
+            [tensor[i] for tensor in self.chain] for i in range(len(self.chain[0]))
+        ]
+
+        return self.chain
+
+
+if __name__ == "__main__":
+    with tf.device("/CPU:0"):
+        layers = [1, 50, 1]
+
+        # Create training data
+        n_train = 1000
+        dims = 1
+        f = lambda x: tf.math.sin(x)
+        x_train = tf.random.normal(shape=(n_train, dims), mean=0.0, stddev=3.0)
+        y_train = f(x_train)
+
+        # Should not be needed.
+        # kernel_prior = tfp.distributions.Normal(loc=0., scale=0.01)
+        # bias_prior = tfp.distributions.Normal(loc=0., scale=0.01)
+
+        num_results = 1000
+        num_burnin_steps = 1000
+        num_leapfrog_steps = 60
+        step_size = 0.01
+        bnn = BayesianNeuralNetworkHMC(layers=layers, activation=tf.nn.sigmoid)
+        chain = bnn.hmc_chain(
+            x=x_train,
+            y=y_train,
+            num_results=num_results,
+            num_burnin_steps=num_burnin_steps,
+            num_leapfrog_steps=num_leapfrog_steps,
+            step_size=step_size,
+        )
+
+        # test the model
+        n_test = 1000
+        x_test = tf.random.normal(shape=(n_test, 1), mean=0.0, stddev=3.0)
+        predictions = bnn.predict_from_chain(x_test)
+
+        x = np.array(list(x_test.numpy().squeeze(-1)) * num_results)
+        predictions = np.array(predictions)
+        predictions = predictions.squeeze(-1).ravel()
+        sns.lineplot(x, predictions, ci="sd")
+
+        x = np.linspace(-2 * np.pi, 2 * np.pi, 1001)
+        plt.plot(x, f(x), label="True function", color="r")
+        plt.scatter(x_train, y_train, label="observed data")
+        plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.show()
--- code/core/bnn_tf.py
+++ code/core/bnn_tf.py
@@ -12,63 +12,12 @@ np.random.seed(1)
 tf.random.set_seed(1)
 
 
-def get_model_bnn(layers, input_shape, activation):
-    model = tf.keras.Sequential()
-    model.add(
-        BayesianDenseLayer(
-            units=layers[0], input_shape=input_shape, activation=activation
-        )
-    )
-    for units in layers[1:-1]:
-        model.add(BayesianDenseLayer(units=units, activation=activation))
-    model.add(BayesianDenseLayer(units=layers[-1], activation=None))
-
-    return model
-
-
-def get_model(layers, input_shape, activation):
-    model = tf.keras.Sequential()
-    model.add(
-        tf.keras.layers.Dense(
-            units=layers[0], input_shape=input_shape, activation=activation
-        )
-    )
-    for units in layers[1:-1]:
-        model.add(tf.keras.layers.Dense(units=units, activation=activation))
-    model.add(tf.keras.layers.Dense(units=layers[-1], activation=None))
-
-    return model
-
-@tf.function
-def loss_fn(model, y, yhat, lamb=1e-3):
-    loss = tf.reduce_sum(tf.keras.losses.MSE(y, yhat))
-    tmp = 0
-    for layer in model.layers:
-        tmp += tf.keras.losses.MSE(0., layer.kernel)
-        tmp += tf.keras.losses.MSE(0., layer.bias)
-    tmp = tf.reduce_sum(tmp)
-    loss += lamb * tmp
-    return loss
-
-@tf.function
-def grad_loss(model, x, y):
-    with tf.GradientTape() as tape:
-        yhat = model(x)
-        loss = loss_fn(model, y, yhat)    
-    grad = tape.gradient(loss, model.trainable_variables)
-    return loss, grad
-
-@tf.function
-def get_kinetic_energy(momenta):
-    K = tf.reduce_sum([tf.reduce_sum(p ** 2) for p in momenta])
-    return K
-
 @tf.function
 def sample_chain(*args, **kwargs):
     return tfp.mcmc.sample_chain(*args, **kwargs)
 
 
-def predict_from_chain(x_test, chain, model):
+def predict_from_chain(x_test, chain):
     predictions = []
     for weights in chain:
         model = build_model(weights)
@@ -76,7 +25,7 @@ def predict_from_chain(x_test, chain, model):
     return predictions
 
 @tf.function
-def prior_log_prob_fn(params, lamb = 0.):
+def prior_log_prob_fn(params, lamb = 1e-3):
     log_prob = 0
     for w in params:
         log_prob -= tf.reduce_sum(w ** 2)
@@ -95,9 +44,7 @@ def bnn_log_prob_fn(X, y, params, get_mean=False):
         tf.tensor: Sum or mean of log probabilities of all labels.
     """
     model = build_model(params)
-    #model.set_weights(params)
     yhat = model(X)
-
     return -0.5 * tf.reduce_sum((y - yhat)**2)
 
 def target_log_prob_fn_factory(X_train, y_train):
@@ -112,8 +59,6 @@ def dense(x, w, b, activation):
     return activation(tf.matmul(x, w) + b)
 
 def build_model(params, activation=tf.nn.sigmoid):
-    
-    
     def model(x):
         kernel = params[::2]
         bias = params[1::2]
@@ -142,19 +87,12 @@ if __name__ == "__main__":
     
     with tf.device("/CPU:0"):
         layers = [1, 20, 1]
-        #input_shape = (1,)
-        #model = get_model(layers, input_shape, activation="sigmoid")
-        #print(model.summary())
-        f = lambda x: tf.math.sin(x)
 
+        f = lambda x: tf.math.sin(x)
         n_train = 1000
         dims = 1
         x_train = tf.random.normal(shape=(n_train, dims), mean=0., stddev=3.)
         y_train = f(x_train)
-
-        #yhat = model(x_train)
-        #print(yhat.shape)
-
 
 
         kernel_prior = tfp.distributions.Normal(loc=0., scale=0.01)
@@ -170,14 +108,6 @@ if __name__ == "__main__":
             tmp.append(kernel)
             tmp.append(bias)
         current_state = tmp
-        for weights in current_state:
-            print(weights.shape)
-
-        model = build_model(current_state)
-        yhat = model(x_train)
-        print(yhat.shape)
-
-
 
         target_log_prob_fn = target_log_prob_fn_factory(x_train, y_train)
 
@@ -197,10 +127,10 @@ if __name__ == "__main__":
         #     step_size=0.01
         # )
 
-        adaptive_kernel = tfp.mcmc.DualAveragingStepSizeAdaptation(
-            inner_kernel=kernel,
-            num_adaptation_steps=1000,
-        )
+        # adaptive_kernel = tfp.mcmc.DualAveragingStepSizeAdaptation(
+        #     inner_kernel=kernel,
+        #     num_adaptation_steps=1000,
+        # )
 
         
         num_results = 1000
@@ -225,7 +155,7 @@ if __name__ == "__main__":
 
         n_test = 1000
         x_test = tf.random.normal(shape=(n_test,1), mean=0., stddev=3.)
-        predictions = predict_from_chain(x_test, chain, model)
+        predictions = predict_from_chain(x_test, chain)
 
         x = np.array(list(x_test.numpy().squeeze(-1))*num_results)
         predictions = np.array(predictions)
@@ -239,38 +169,3 @@ if __name__ == "__main__":
         plt.xlabel("x")
         plt.ylabel("y")
         plt.show()
-
-
-    # with tf.device("/CPU:0"):
-    #     model, samples = bayesian_fit(
-    #         model,
-    #         x_train,
-    #         y_train,
-    #         L=60,
-    #         eps=0.0001,
-    #         num_burnin_steps=num_burnin_steps,
-    #         num_samples=num_results,
-    #     )
-
-    #     #print(f"{samples=}")
-    #     n_test = 1000
-    #     x_test = tf.random.normal(shape=(n_test, dims), mean=0., stddev=2.)
-    #     y_test = f(x_test)
-
-    #     predictions = []
-    #     for weights in samples:
-    #         model.set_weights(weights)
-    #         predictions.append(model(x_test).numpy().ravel())
-
-    #     predictions = np.array(predictions)
-    #     predictions = predictions.ravel()
-
-    #     X = np.array(list(x_test) * num_results).squeeze(-1)
-    #     sns.lineplot(x=X, y=predictions, ci="sd")
-
-    #     x = np.linspace(-2 * np.pi, 2 * np.pi, 1001)
-    #     plt.plot(x, f(x), label="True function", color="r")
-    #     plt.legend()
-    #     plt.xlabel("x")
-    #     plt.ylabel("y")
-    #     plt.show()
