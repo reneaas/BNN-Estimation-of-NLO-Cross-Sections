26f45a95706b20c3b63e5155ebf0ce0a5e4ef947
reneaas <reneaas@uio.no>
2022-06-07 00:29:56 +0200

    update

--- code/core/posterior_weights.py
+++ code/core/posterior_weights.py
@@ -7,7 +7,7 @@ from slha_loader.slha_loader import SLHALoader
 from utils.preprocessing import split_data
 
 def main():
-    model_name = r"models/3_small_hidden_layers.npz"
+    model_name = r"models/2_small_hidden_layers.npz"
 
     bnn = BayesianNeuralNetwork()
     bnn.load_model(fname=model_name)
@@ -17,11 +17,16 @@ def main():
     print([p.shape for p in weights])
 
     i = 2
-    j = 5
+    j = 3
     l = 0
     w = weights[l][:, i, j].numpy().ravel()
     # sns.histplot(w)
     plt.hist(w, bins=100, histtype="step")
+    plt.show()
+
+
+    # 2D-distribution plots.
+    sns.displot(x=weights[l][:, i, j].numpy(), y=weights[l][:, i, j-1].numpy(), kind="kde")
     plt.show()
 
 
